e11cce85285e704992700c5bc72d0244d5030c03
foxxyloxxy <foxxyloxxy580@gmail.com>
2022-03-21 10:21:21 -0400

    lol idk

--- client/src/index.js
+++ client/src/index.js
@@ -6,30 +6,31 @@ import Book from './components/Book';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <Book color="rgb(255, 223, 186)" pages={[
-  {id: 0, title: "Biology", cards: [
-    {id: 0, title: "Mitochondria", color: "rgb(255,179,186)", pos: {
-      x: 500, y: 500
-    }, value: "", termlistValue: ""},
-    {id: 1, title: "Chloroplast", color: "rgb(186,255,201)", pos: {
-      x: 1300, y: 200
-    }, value: "", termlistValue: ""},
-    {id: 2, title: "Nucleus", color: "rgb(186,225,255)", pos: {
-      x: 40, y: 30
-    }, value: "", termlistValue: ""}
-  ], lines: [[0, 1], [0, 2], [1, 2]]}, 
-  {id: 1, title: "Chemistry", cards: [
-    {id: 0, title: "Elements", color: "rgb(255,179,186)", pos: {
-      x: 500, y: 500
-    }, value: "", termlistValue: ""},
-    {id: 1, title: "Compounds", color: "rgb(186,255,201)", pos: {
-      x: 1300, y: 200
-    }, value: "", termlistValue: ""},
-    {id: 2, title: "Experiments", color: "rgb(186,225,255)", pos: {
-      x: 40, y: 30
-    }, value: "", termlistValue: ""}
-  ], lines: []}
-  ]}
+  <Book 
+  // color="rgb(255, 223, 186)" pages={[
+  // {id: 0, title: "Biology", cards: [
+  //   {id: 0, title: "Mitochondria", color: "rgb(255,179,186)", pos: {
+  //     x: 500, y: 500
+  //   }, value: "", termlistValue: ""},
+  //   {id: 1, title: "Chloroplast", color: "rgb(186,255,201)", pos: {
+  //     x: 1300, y: 200
+  //   }, value: "", termlistValue: ""},
+  //   {id: 2, title: "Nucleus", color: "rgb(186,225,255)", pos: {
+  //     x: 40, y: 30
+  //   }, value: "", termlistValue: ""}
+  // ], lines: [[0, 1], [0, 2], [1, 2]]}, 
+  // {id: 1, title: "Chemistry", cards: [
+  //   {id: 0, title: "Elements", color: "rgb(255,179,186)", pos: {
+  //     x: 500, y: 500
+  //   }, value: "", termlistValue: ""},
+  //   {id: 1, title: "Compounds", color: "rgb(186,255,201)", pos: {
+  //     x: 1300, y: 200
+  //   }, value: "", termlistValue: ""},
+  //   {id: 2, title: "Experiments", color: "rgb(186,225,255)", pos: {
+  //     x: 40, y: 30
+  //   }, value: "", termlistValue: ""}
+  // ], lines: []}
+  // ]}
   />,
 
 // <Page title = "Biology" cards = {[
